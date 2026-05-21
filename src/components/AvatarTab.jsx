edc2ca9f8733f7fce3da0a5985894864bc5d8cc0
Student <student@example.com>
2026-05-21 17:32:39 +0800

    Much smaller avatar - calc(100vh-300px)

--- src/components/AvatarTab.jsx
+++ src/components/AvatarTab.jsx
@@ -61,7 +61,7 @@ export default function AvatarTab() {
       )}
 
       {state === 'active' && embedUrl && (
-        <div className="rounded-2xl overflow-hidden border border-crm-border/50 bg-black h-[calc(100vh-180px)]">
+        <div className="rounded-2xl overflow-hidden border border-crm-border/50 bg-black h-[calc(100vh-300px)]">
           <iframe
             src={embedUrl}
             className="w-full h-full border-0"
